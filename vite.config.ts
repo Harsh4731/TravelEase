9930e52780e456f8acd82b689321f73d9c1645ce
Harsh4731 <harshsolanki4731@gmail.com>
2025-11-04 20:36:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { componentTagger } from "lovable.tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // 👇 Ye line add karni hai (important for GitHub Pages)
+  base: "/globe-trek-interface/",
+
   server: {
-    host: "::",
+    host: "*",
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
@@ -16,3 +19,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
